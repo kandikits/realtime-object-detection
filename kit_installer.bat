8fec1b3b27d823a4e4f61e3f0ca4e877de5dff28
kandikits <95465112+kandikits@users.noreply.github.com>
2021-12-14 13:09:55 +0530

    Add files via upload

---
+++ kit_installer.bat
@@ -0,0 +1,63 @@
+@echo off
+REM This is a sample script
+setlocal ENABLEDELAYEDEXPANSION
+REM update below path if required
+SET PY_LOCATION="C:\Python"
+where /q python
+IF ERRORLEVEL 1 (
+    ECHO Python is not found in PATH variable
+	curl -o python-3.9.8-amd64.exe https://www.python.org/ftp/python/3.9.8/python-3.9.8-amd64.exe
+	python-3.9.8-amd64.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0 TargetDir=%PY_LOCATION%
+	ECHO Installing python3.9.8 ...
+	ECHO Python installed in path : %PY_LOCATION%
+	IF ERRORLEVEL 1 (
+    		ECHO "There was an error while installing python!"
+    		EXIT /B
+	) ELSE (
+		curl -o requirements.txt https://raw.githubusercontent.com/kandikits/realtime-object-detection/main/requirements.txt
+		%PY_LOCATION%\python.exe -m pip install -r requirements.txt
+	)
+    EXIT /B
+) ELSE (
+	ECHO Python is found in PATH variable
+	for /f %%i in ('python -c "import sys; print(sys.version_info[0])"') do set PYTHON_M_VERSION=%%i	
+	ECHO !PYTHON_M_VERSION!
+	IF !PYTHON_M_VERSION! EQU 2 (
+		ECHO python3 will be installed since the version of existing python is 2
+		curl -o python-3.9.8-amd64.exe https://www.python.org/ftp/python/3.9.8/python-3.9.8-amd64.exe
+		python-3.9.8-amd64.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0 TargetDir=%PY_LOCATION%
+		ECHO Installing python3.9.8 ...
+		ECHO Python installed in path : %PY_LOCATION%
+		IF ERRORLEVEL 1 (
+			ECHO There was an error while installing python!
+			EXIT /B
+		) ELSE (
+			ECHO Installing dependent modules ...
+			curl -o requirements.txt https://raw.githubusercontent.com/kandikits/realtime-object-detection/main/requirements.txt
+			%PY_LOCATION%\python.exe -m pip install -r requirements.txt
+		)
+
+	) ELSE (
+		IF !PYTHON_M_VERSION! EQU 3 (
+			ECHO Installing dependent modules
+			curl -o requirements.txt https://raw.githubusercontent.com/kandikits/realtime-object-detection/main/requirements.txt
+			python -m pip install -r requirements.txt
+		) else (
+			ECHO Python found wasn't installed
+			curl -o python-3.9.8-amd64.exe https://www.python.org/ftp/python/3.9.8/python-3.9.8-amd64.exe
+			python-3.9.8-amd64.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0 TargetDir=%PY_LOCATION%
+			ECHO Installing python3.9.8 ...
+			ECHO Python installed in path : %PY_LOCATION%
+			IF ERRORLEVEL 1 (
+				ECHO There was an error while installing python!
+				EXIT /B
+			) ELSE (
+				ECHO Installing dependent modules
+				curl -o requirements.txt https://raw.githubusercontent.com/kandikits/realtime-object-detection/main/requirements.txt
+				%PY_LOCATION%\python.exe -m pip install -r requirements.txt
+			)
+		)	
+	)
+)
+
+curl -o realtime-object-detection.zip https://github.com/kandikits/realtime-object-detection/archive/refs/heads/main.zip
