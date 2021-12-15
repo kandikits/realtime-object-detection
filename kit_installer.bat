d316f7c657b72d2120f850eff2147646023eab56
kandikits <95465112+kandikits@users.noreply.github.com>
2021-12-15 13:27:45 +0530

    Add files via upload

--- kit_installer.bat
+++ kit_installer.bat
@@ -1,5 +1,11 @@
 @echo off
 REM This is a sample script
+ECHO======================================================================================
+ECHO		Kandi kit installation process has begun
+ECHO======================================================================================
+ECHO 	This kit installer works only on operating systems - Windows 10 and above
+ECHO 	Based on your network speed, the installation may take a while
+ECHO======================================================================================
 setlocal ENABLEDELAYEDEXPANSION
 REM update below path if required
 SET PY_LOCATION="C:\Python"
@@ -7,45 +13,81 @@ SET PY_VERSION=3.9.8
 SET PY_DOWNLOAD_URL=https://www.python.org/ftp/python/3.9.8/python-3.9.8-amd64.exe
 where /q python
 IF ERRORLEVEL 1 (
+	ECHO==========================================================================
     ECHO Python wasn't found in PATH variable
+	ECHO==========================================================================
 	CALL :Install_python_and_modules
-    EXIT /B
+	IF ERRORLEVEL 1 (
+		PAUSE
+	) ELSE (
+		CALL :Download_repo
+	)
 ) ELSE (
-	for /f %%i in ('python -c "import sys; print(sys.version_info[0])"') do set PYTHON_M_VERSION=%%i	
-	ECHO !PYTHON_M_VERSION!
+	for /f %%i in ('python -c "import sys; print(sys.version_info[0])"') do set PYTHON_M_VERSION=%%i
+	REM ECHO The major version of python was "!PYTHON_M_VERSION!"
 	IF !PYTHON_M_VERSION! EQU 2 (
+		ECHO==========================================================================
 		ECHO python3 will be installed since the version of existing python is 2
+		ECHO==========================================================================
 		CALL :Install_python_and_modules
+		IF ERRORLEVEL 1 (
+			PAUSE
+		) ELSE (
+			CALL :Download_repo
+		)
 	) ELSE (
 		IF !PYTHON_M_VERSION! EQU 3 (
+			ECHO==========================================================================
 			ECHO A valid python is detected and hence installing dependent modules ...
+			ECHO==========================================================================
 			curl -o requirements.txt https://raw.githubusercontent.com/kandikits/realtime-object-detection/main/requirements.txt
 			python -m pip install -r requirements.txt
+			CALL :Download_repo
 		) else (
+			ECHO==========================================================================
 			ECHO Python wasn't detected!
+			ECHO==========================================================================
 			CALL :Install_python_and_modules
+			IF ERRORLEVEL 1 (
+				PAUSE
+			) ELSE (
+				CALL :Download_repo
+			)
 		)	
 	)
 )
-
-curl -o realtime-object-detection.zip https://codeload.github.com/kandikits/realtime-object-detection/zip/refs/heads/main
-ECHO Kit has been installed successfully
 EXIT /B %ERRORLEVEL%
+REM PAUSE
+
+:Download_repo
+curl -o realtime-object-detection.zip https://codeload.github.com/kandikits/realtime-object-detection/zip/refs/heads/main
+ECHO==========================================================================
+ECHO 	The Kit has been installed successfully
+ECHO==========================================================================
+ECHO 	To run the kit, follow further instructions of the kit in kandi	
+ECHO==========================================================================
+EXIT /B 0
 
 :Install_python_and_modules
+ECHO==========================================================================
 ECHO Downloading python%PY_VERSION% ... 
+ECHO==========================================================================
 curl -o python-%PY_VERSION%-amd64.exe %PY_DOWNLOAD_URL%
-python-%PY_VERSION%-amd64.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0 TargetDir=%PY_LOCATION%
 ECHO Installing python%PY_VERSION% ...
+python-%PY_VERSION%-amd64.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0 TargetDir=%PY_LOCATION%
+ECHO==========================================================================
 ECHO Python installed in path : %PY_LOCATION%
+ECHO==========================================================================
 IF ERRORLEVEL 1 (
-		ECHO "There was an error while installing python!"
-		EXIT /B
+		ECHO==========================================================================
+		ECHO There was an error while installing python!
+		ECHO==========================================================================
+		EXIT /B 1
 ) ELSE (
+	ECHO==========================================================================
 	ECHO Installing dependent modules ...
 	curl -o requirements.txt https://raw.githubusercontent.com/kandikits/realtime-object-detection/main/requirements.txt
 	%PY_LOCATION%\python.exe -m pip install -r requirements.txt
+	ECHO==========================================================================
 )
 EXIT /B 0
-
-PAUSE
